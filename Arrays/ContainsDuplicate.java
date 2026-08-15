e08c28e2e79263572a4d7554978530ad492d76fb
samthegamer-007 <gopesambit2009@gmail.com>
2026-08-15 17:30:22 +0530

    Add duplicate detection method in ContainsDuplicate
    Implement method to check for duplicate values in an array.

---
+++ Arrays/ContainsDuplicate.java
@@ -0,0 +1,23 @@
+//Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+//Solution
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        int l= nums.length;
+        for (int i=0; i<l; i++) 
+        {
+           for (int j = 0; j<l;j++)
+           {
+            if (i==j)
+            continue;
+            else
+            {
+                if(nums[i]==nums[j])
+                return true;
+            }
+           } 
+
+
+        }
+        return false;
+    }
+}
