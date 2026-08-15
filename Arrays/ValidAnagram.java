206da958d6dbfc89b8972ec6b295e0b3dc484381
samthegamer-007 <gopesambit2009@gmail.com>
2026-08-15 17:32:36 +0530

    Add ValidAnagram class with isAnagram method
    Implement a method to check if two strings are anagrams.

---
+++ Arrays/ValidAnagram.java
@@ -0,0 +1,21 @@
+//Given two strings s and t, return true if the two strings are anagrams of each other, otherwise return false.
+//An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+//SOLUTION
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int sl= s.length(), tl= t.length();
+        if(tl!=sl)
+        return false;
+        else
+        {
+            char[] sSort = s.toCharArray();
+            char[] tSort= t.toCharArray();
+            Arrays.sort(sSort);
+            Arrays.sort(tSort);
+            return Arrays.equals(sSort,tSort);
+
+
+        }
+
+    }
+}
