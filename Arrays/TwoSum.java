610b893cfa2fd2b04d241be7a2ec3120084e216e
samthegamer-007 <gopesambit2009@gmail.com>
2026-08-15 17:34:01 +0530

    Add twoSum method in TwoSum.java
    Implement twoSum method to find indices of two numbers that add up to target.

---
+++ Arrays/TwoSum.java
@@ -0,0 +1,19 @@
+//Given an array of integers nums and an integer target, return the indices i and j such that nums[i] + nums[j] == target and i != j.
+//You may assume that every input has exactly one pair of indices i and j that satisfy the condition.
+//Return the answer with the smaller index first.
+//SOLUTION
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+       for (int i= 0; i<nums.length;i++)
+       {
+        for (int j= 0;j<nums.length;j++)
+        {
+            if (i==j)
+            continue;
+            if ((nums[i]+nums[j])==target)
+            return  new int[]{i,j};
+        }
+       } 
+       return new int[0];
+    }
+}
